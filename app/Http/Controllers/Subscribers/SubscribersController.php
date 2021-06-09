8894506982c58a4c950b5491b04aea2e5a5a554b
enricouniurb <enrico.oliva@uniurb.it>
2021-06-09 10:58:47 +0200

    feat: export

--- app/Http/Controllers/Subscribers/SubscribersController.php
+++ app/Http/Controllers/Subscribers/SubscribersController.php
@@ -8,13 +8,20 @@ use App\Http\Middleware\OwnsCurrentWorkspace;
 use Sendportal\Base\Repositories\Subscribers\SubscriberTenantRepositoryInterface;
 use Sendportal\Base\Repositories\TagTenantRepository;
 use Sendportal\Base\Http\Controllers\Subscribers\SubscribersController as BaseSubscribersController;
+use Rap2hpoutre\FastExcel\FastExcel;
+use Sendportal\Base\Facades\Sendportal;
 
 class SubscribersController extends BaseSubscribersController
 {    
 
+    /** @var SubscriberTenantRepositoryInterface */
+    private $subscriberRepo;
+
     public function __construct(SubscriberTenantRepositoryInterface $subscriberRepo, TagTenantRepository $tagRepo)
     {
         parent::__construct($subscriberRepo, $tagRepo);
+
+        $this->subscriberRepo = $subscriberRepo;
 
         $this->middleware(OwnsCurrentWorkspace::class)->only([
             'create',
@@ -23,6 +30,37 @@ class SubscribersController extends BaseSubscribersController
             'update',
             'destroy'
         ]);
+    }
+
+    /**
+     * @return string|StreamedResponse
+     * @throws IOException
+     * @throws InvalidArgumentException
+     * @throws UnsupportedTypeException
+     * @throws WriterNotOpenedException
+     * @throws Exception
+     */
+    public function export()
+    {
+        $subscribers = $this->subscriberRepo->all(Sendportal::currentWorkspaceId(), 'id');
+
+        if (!$subscribers->count()) {
+            return redirect()->route('sendportal.subscribers.index')->withErrors(__('There are no subscribers to export'));
+        }
+
+        return (new FastExcel($subscribers))
+            ->download(sprintf('subscribers-%s.csv', date('Y-m-d-H-m-s')), static function ($subscriber) {
+                return [
+                    'id' => $subscriber->id,
+                    'hash' => $subscriber->hash,
+                    'email' => $subscriber->email,
+                    'first_name' => $subscriber->first_name,
+                    'last_name' => $subscriber->last_name,
+                    'text1' => $subscriber->text1,
+                    'text2' => $subscriber->text2,
+                    'created_at' => $subscriber->created_at,
+                ];
+            });
     }
 
 }
